b9f0d31804d34656e8c59ece06b7f050b21a3808
ntalbs <ntalbsen@gmail.com>
2023-01-21 21:14:59 +0000

    Change variable name from (upper_left, lower_right) to (top_left, bottom_right)

--- src/main.rs
+++ src/main.rs
@@ -42,16 +42,16 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
 fn pixel_to_point(
     bounds: (usize, usize),
     pixel: (usize, usize),
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
+    top_left: Complex<f64>,
+    bottom_right: Complex<f64>,
 ) -> Complex<f64> {
     let (width, height) = (
-        lower_right.re - upper_left.re,
-        upper_left.im - lower_right.im
+        bottom_right.re - top_left.re,
+        top_left.im - bottom_right.im
     );
     Complex {
-        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        re: top_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: top_left.im - pixel.1 as f64 * height / bounds.1 as f64
     }
 
 }
@@ -59,14 +59,14 @@ fn pixel_to_point(
 fn render(
     pixels: &mut [u8],
     bounds: (usize, usize),
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
+    top_left: Complex<f64>,
+    bottom_right: Complex<f64>,
 ) {
     assert!(pixels.len() == bounds.0 * bounds.1);
 
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            let point = pixel_to_point(bounds, (column, row), top_left, bottom_right);
             pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - count as u8,
@@ -84,17 +84,17 @@ fn write_image(fliename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 5 {
-        eprintln!("Usage: {} FILE PIXELS_WIDTH UPPER_LEFT LOWER_RIGHT", args[0]);
+        eprintln!("Usage: {} FILE PIXELS_WIDTH TOP_LEFT BOTTOM_RIGHT", args[0]);
         eprintln!("Exapple: {} mandel.png 2000 -1.20,0.35 -1,0.20", args[0]);
         std::process::exit(1);
     }
 
     let width_px = parse_width(&args[2]).expect("error parsing image width");
-    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let top_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let bottom_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
 
-    let width = lower_right.re - upper_left.re;
-    let height = upper_left.im - lower_right.im;
+    let width = bottom_right.re - top_left.re;
+    let height = top_left.im - bottom_right.im;
 
     let ratio = width_px as f64 / width;
     let height_px = (height * ratio) as usize;
@@ -104,6 +104,6 @@ fn main() {
     println!(">>height_px: {}", height_px);
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+    render(&mut pixels, bounds, top_left, bottom_right);
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
